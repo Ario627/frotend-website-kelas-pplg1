2f40a3dfbcfc72f8b2fbfdd8f8898854b1aad0c7
Ario <arioabdillah5@gmail.com>
2026-02-11 13:09:17 +0700

    semoga api nya ga ngawur

--- src/lib/api/client.ts
+++ src/lib/api/client.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosError, InternalAxiosRequestConfig } from "axios";
 
 
-const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
+const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL + "/api";
 
 export const apiClient = axios.create({
   baseURL: API_BASE_URL,
